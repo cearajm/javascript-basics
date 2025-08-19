ce1dbbdbaefa670b2ea5f3dbdc4796b8fb7f777c
ceara <cearamahinay@gmail.com>
2025-08-19 13:46:17 -0600

    Display UI

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -23,8 +23,8 @@ function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toUpperCase();
     computerChoice = computerChoice.toUpperCase();
 
-    console.log("you: " + humanChoice);
-    console.log("robot: " + computerChoice);
+    // console.log("you: " + humanChoice);
+    // console.log("robot: " + computerChoice);
 
     let win;
     if (humanChoice === computerChoice) {
@@ -59,59 +59,77 @@ let rock = document.querySelector("rock");
 let paper = document.querySelector("paper");
 let scissors = document.querySelector("scissors");
 
+let continuePlaying = true;
+let humanScore = 0;
+let computerScore = 0;
+let winnerMessage = "";
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach(item => {
     const humanChoice = item.className;
     item.addEventListener("click", () => {
-        let win = playRound(humanChoice, getComputerChoice());
-        winMessage(win);
-        });
+        if (continuePlaying) {
+            let win = playRound(humanChoice, getComputerChoice());
+            winMessage(win);
+            if (humanScore == 5 || computerScore == 5) {
+                continuePlaying = false;
+                // console.log("game over bye bye");
+                determineWinner(humanScore, computerScore);
+                displayWinner.textContent = winnerMessage + " game over bye bye";
+            }
+        }
+        })
     }
 );
 
-function winMessage(win) {
-    if (win == "tie") {
-        console.log("it's a tie. how boring");
-    } else if (win) {
-        console.log("winner winner");
+
+function determineWinner(humanScore, computerScore) {
+    if (humanScore > computerScore) {
+        winnerMessage= "YOU WIN THE GAME AAAAA";
     } else {
-        console.log("wow embarassing");
+        winnerMessage = "oof. never show your face here again.";
     }
 }
 
-// rock.addEventListener("click")
+let resultsMessage = "";
+
+function winMessage(win) {
+    if (win == "tie") {
+        // console.log("it's a tie. how boring");
+        resultsMessage = "it's a tie. how boring";
+    } else if (win) {
+        // console.log("winner winner");
+        resultsMessage = "winner winner";
+        humanScore++;
+        humanScoreText.textContent = "your score: " + humanScore;
+    } else {
+        // console.log("you smelly loser you're a freak");
+        resultsMessage = "you smelly loser you're a freak";
+        computerScore++;
+        computerScoreText.textContent = "computer score: " + computerScore;
+    }
+    results.textContent = resultsMessage;
+    // console.log("human score: " + humanScore);
+    // console.log("computer score: " + computerScore);
+}
+
+const container = document.querySelector("body");
+const results = document.createElement("div");
+const scoreContainer = document.querySelector("body");
+const humanScoreText = document.createElement("div");
+const computerScoreText = document.createElement("div");
+const displayWinner = document.createElement("div");
+
+container.appendChild(results);
+scoreContainer.appendChild(humanScoreText);
+scoreContainer.appendChild(computerScoreText);
+scoreContainer.appendChild(displayWinner);
+
+results.textContent = "game start !! good luck everybuddy";
+humanScoreText.textContent = "your score: " + humanScore;
+computerScoreText.textContent = "computer score: " + computerScore;
 
 
-
-
-// let win = playRound(getHumanChoice(), getComputerChoice());
-
-
-
-
-// PLAY FIVE ROUNDS
-// let humanScore = 0;
-// let computerScore = 0;
-
-// for (let i = 0; i < 5; i++) {
-//     let win = playRound(getHumanChoice(), getComputerChoice(3));
-
-//     if (win == "tie") {
-//     } else if (win == false) {
-//         console.log();
-//         computerScore++;
-//     } else if (win == true) {
-//         humanScore++;
-//     }
-// }
-
-// console.log("your score: " + humanScore);
-// console.log("computer score: " + computerScore);
-
-// if (humanScore > computerScore) {console.log("yaaaay winner");}
-// else if (humanScore < computerScore) {console.log("you smelly loser you're a freak");}
-// else {console.log("it's a tie");}
 
 
 
