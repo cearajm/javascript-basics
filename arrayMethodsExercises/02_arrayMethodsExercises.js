1b864c66d52ed97c38d395eaf1a946173684892d
ceara <cearamahinay@gmail.com>
2025-08-21 16:50:59 -0600

    Map to objects

--- arrayMethodsExercises/02_arrayMethodsExercises.js
+++ arrayMethodsExercises/02_arrayMethodsExercises.js
@@ -6,16 +6,37 @@ let pete = { name: "Pete", age: 30 };
 let mary = { name: "Mary", age: 28 };
 
 let users = [ john, pete, mary ];  // array of user objects, convert to array of names
-
 let names = users.map(getName);
-alert( names ); // John, Pete, Mary
-
+// alert( names ); // John, Pete, Mary
 
 function getName(name) {
     return name["name"];
 };
 
+
+
 // map to objects
+let leon = { name: "Leon", surname: "Smith", id: 1 };
+let cione = { name: "Cione", surname: "Hunt", id: 2 };
+let puppet = { name: "Puppet", surname: "Key", id: 3 };
+
+let friends = [ leon, cione, puppet ];
+
+let friendsMapped = friends.map(item => ({  // returns new array, original array untouched
+    fullName: `${item.name} ${item.surname}`,
+    id: item.id,
+}));
+
+// console.log(friendsMapped);
+// alert( friendsMapped[0].id ) // 1
+// alert( friendsMapped[0].fullName ) // John Smith
+
+
 // sort users by age
+
+
+
+
+
 // get average age
 // create keyed object from array
